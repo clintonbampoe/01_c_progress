8b1da4b32af8ae34ccfc6ed96e4dc153aab903fb
Clinton Bampoe <clintbamp@gmail.com>
2025-08-25 21:27:36 +0000

    Exercise 4 Complete, chapter 9 functions

---
+++ ch09_functions/00_temp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int day_of_year(int, int, int);
+
+int main(void) {
+
+    int day_num = day_of_year(12,31,2025);
+    printf("%d", day_num);
+}
+
+int day_of_year(int month, int day, int year) {
+    int days_in_month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int total_days = 0;
+
+    // if year is leap year
+    if ((year % 4 == 0) || (year % 100 == 0 && year % 400 == 0)) {
+        // feb is 29 days
+        days_in_month[1] += 1;
+    }
+
+    // add all days in months month in date
+    // NB: Arrays are zero indexed
+    for (int i = 0; i < (month - 1); i++) {
+        total_days += days_in_month[i];
+    }
+
+    total_days += day;
+    return total_days;
+}
